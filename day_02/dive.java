c2cef6884af08183a88af3a6c902645917e93ea8
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2021-12-12 00:57:40 +0530

    Added solution for puzzle 2 day 2

--- day_02/dive.java
+++ day_02/dive.java
@@ -20,9 +20,30 @@ public class dive {
     return forward * depth;
   }
 
+  long puzzle2() throws IOException {
+    long forward = 0;
+    long depth = 0;
+    long aim = 0;
+    BufferedReader br = new BufferedReader(new FileReader("day_02/direction.list"));
+    String line;
+    while((line = br.readLine()) != null) {
+      if(line.startsWith("f")) {
+        long x = Long.parseLong(line.split(" ")[1]);
+        forward += x;
+        depth += aim * x;
+      } else if (line.startsWith("d")) {
+        aim += Long.parseLong(line.split(" ")[1]);;
+      } else {
+        aim -= Long.parseLong(line.split(" ")[1]);;
+      }
+    }
+    return forward * depth;
+  }
+
   public static void main(String[] args) {
     try {
       System.out.println("Puzzle 1 solution = " + new dive().puzzle1());
+      System.out.println("Puzzle 2 solution = " + new dive().puzzle2());
     } catch (IOException e) {
       e.printStackTrace();
     }
